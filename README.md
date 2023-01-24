a78bb1a9a5f6987138b7286b8bd3b7acf7f29e3a
Grégory Copin <gregcop1@gmail.com>
2023-01-24 17:05:43 +0100

    feat: Ajout de l'énoncé de l'atelier n°1

--- README.md
+++ README.md
@@ -3,4 +3,18 @@ Exercices dédiés à la formation d'introduction à React.
 
 > Note : clonez ce repository pour pouvoir participer aux ateliers
 
-> :warning: Node 14+ requis. 
+> :warning: Node 14+ requis.
+
+## Préalable à l'atelier
+
+- Installez le projet : `npm install`
+- Lancez le server : `npm start`
+
+## Atelier 2
+
+Le but de cet exercice est d'afficher une liste de livres en offrant la possibilité à l'utilisateur de les ordonner.
+
+Pour ce faire : 
+ 
+- modifier le composant de `List` pour afficher les livres contenus dans la variable `books` (informations à afficher : isbn, titre et auteur)
+- rendre la liste ordonnable par isbn, titre et auteur
---
+++ src/features/book-list/BookList.js
@@ -0,0 +1,19 @@
+const books = [
+    {isbn: '9793083416011', title: 'Excepturi explicabo volu…am aut voluptate velit.', author: 'Corbin Ritchie'},
+    {isbn: '9786451149666', title: 'Est est et eveniet porro non.', author: 'Deonte Effertz'},
+    {isbn: '9782736956844', title: 'Tempore ab quis impedit itaque assumenda aut.', author: 'Dr. Abel Gutmann IV'},
+    {
+        isbn: '9790275158155',
+        title: 'Quibusdam quia aut culpa dolores consectetur quod doloremque.',
+        author: 'Prof. Scotty Pfannerstill DVM'
+    },
+    {isbn: '9789899956537', title: 'Qui accusamus et odio explicabo quam accusamus.', author: 'Ms. Alana Murphy'},
+];
+
+const BookList = () => {
+    return (
+        <h1>Livres</h1>
+    );
+};
+
+export default BookList;
